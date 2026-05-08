4f00180c22eaae2416512ba710a9b4347601a725
Ivan <83720387+Dufi1928@users.noreply.github.com>
2026-05-08 19:38:23 +0200

    chore: switch Astro adapter from node to netlify and update deployment artifact path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   site: 'https://dufi1928.github.io',
   base: '/custos-avocats',
   output: 'static',
-  adapter: node({ mode: 'standalone' }),
+  adapter: netlify(),
 });
