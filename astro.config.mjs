6815b8a6c03aa2ead89c60861708979c45de8174
Ivan <83720387+Dufi1928@users.noreply.github.com>
2026-05-08 19:39:45 +0200

    chore: remove base path configuration and update API endpoint route

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 
 export default defineConfig({
-  site: 'https://dufi1928.github.io',
-  base: '/custos-avocats',
   output: 'static',
   adapter: netlify(),
 });
